ebf8d04408a2b2f3ff1d8d80a02d6cce34d200c7
tanmay-ninawe <tanmay.ninawe72@gmail.com>
2026-08-19 00:03:33 +0530

    GG

---
+++ D1 - Intro to python and data types & Ai awarenenss.py
@@ -0,0 +1,13 @@
+
+#  Question 1: Write a python program to concatenate two numbers.
+
+# #############################################################################
+
+def main():
+	valueOne = int(input("Enter the Value for A : "))
+	valueTwo = int(input("Enter the Value for B : "))
+	print("Concatenation is : ", str(valueOne) + str(valueTwo))
+
+if __name__ == "__main__":
+	main()
+	
